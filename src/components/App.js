4ee7f14b4233a95ebd000ee9b6133641dcf0a22a
rodrigo.resque <rodrigo.resque@mkdata.com.br>
2023-01-27 17:00:29 -0300

    updt

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Post from "./Post";
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello World!</h1>
-                <h2>Meu primeiro app.</h2>
-            </div>
-        );
-
-    }
+function App() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <h2>Meu primeiro app.</h2>
+        </div>
+    );
 }
+
+export default App
