a7b40f1e6c208fcf83af6a13f724552c2a0c5c5f
rodrigo.resque <rodrigo.resque@mkdata.com.br>
2023-01-30 21:49:18 -0300

    Fragment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './index.css'
 import SayMayName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
+import List from "./components/List";
 
 function App() {
     const nome = 'fulano';
@@ -17,6 +18,7 @@ function App() {
             <SayMayName name="Mattheus" />
             <SayMayName name={nome} />
             <Pessoa foto="https://via.placeholder.com/150" nome="Rodrigo" idade="34" profissao="Programador"/>
+            <List />
         </div>
     );
 }
